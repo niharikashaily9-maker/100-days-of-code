ff5f812e9fb89bb2d0a35731ad40679683019789
niharikashaily9-maker <niharikashaily9@gmail.com>
2026-09-14 13:29:11 +0530

    Create q30.c

---
+++ Day 15/q30.c
@@ -0,0 +1,28 @@
+/Q30: Write a program to reverse a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4321
+
+Input 2:
+100
+Output 2:
+1
+
+*/#include<stdio.h>
+int main()
+{
+    int n,reverse=0,digits;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    while(n!=0){
+        digits=n%10;
+        reverse=reverse*10 +digits;
+        n=n/10;
+    }
+    printf("Reversed numbers are %d",reverse);
+    return 0;
+}

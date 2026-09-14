748104758c856c877305f3ad25ba5f63a1011652
niharikashaily9-maker <niharikashaily9@gmail.com>
2026-09-14 13:06:41 +0530

    Create q5.c

---
+++ Day 3/q5.c
@@ -0,0 +1,25 @@
+//Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/#include<stdio.h>
+int main()
+{
+   float c,f;
+    printf("Enter temp in celcius: ");
+    scanf("%f", &c);
+    f=(9.0/5.0)*c +32;
+    printf("Temp in faherenheit=%.2f\n",f);
+    return 0;
+
+}
